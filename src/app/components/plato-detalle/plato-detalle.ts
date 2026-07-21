a2ad498ec9c85da97a236a79ba71e38db054e5b0
Maxito06 <maxicopi5@gmail.com>
2026-07-21 15:39:02 -0500

    feat: implementar lógica de PlatoDetalleComponent con búsqueda por id

--- src/app/components/plato-detalle/plato-detalle.ts
+++ src/app/components/plato-detalle/plato-detalle.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { MenuService } from '../../services/menu';
 
 @Component({
   selector: 'app-plato-detalle',
-  imports: [],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './plato-detalle.html',
   styleUrl: './plato-detalle.scss',
 })
-export class PlatoDetalle {}
+export class PlatoDetalle {
+  private menuService = inject(MenuService);
+  private route = inject(ActivatedRoute);
+
+  cargando = this.menuService.cargando;
+  error = this.menuService.error;
+
+  plato = computed(() => {
+    const id = this.route.snapshot.paramMap.get('id');
+    return this.menuService
+      .menuData()
+      ?.categorias.flatMap((categoria) => categoria.platos)
+      .find((plato) => plato.id === id);
+  });
+}
