ac17d594ae7dbae6197b1a12c529c97471f66838
Maxito06 <maxicopi5@gmail.com>
2026-07-21 15:37:38 -0500

    feat: implementar lógica de MenuComponent con carga de categorías y platos

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { MenuService } from '../../services/menu';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
-export class Menu {}
+export class Menu {
+  private menuService = inject(MenuService);
+
+  menuData = this.menuService.menuData;
+  cargando = this.menuService.cargando;
+  error = this.menuService.error;
+}
